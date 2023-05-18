08a7a42efb42a65bb94708dfe76383958eaf088e
Luna Borella <ulfrthewolf@gmail.com>
2023-05-19 00:56:44 +1000

    Changed around the project structure a little

--- benches/parser_benchmark.rs
+++ benches/parser_benchmark.rs
@@ -2,24 +2,14 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use taiko::parser::{parse_tja_file, tja_file_bench};
 
 pub fn tja_benchmark(c: &mut Criterion) {
-    let ready_to = include_str!("../example-tracks/Ready To/Ready to.tja");
+    let ready_to = include_str!("../example-tracks/Ready to/Ready to.tja");
     c.bench_function("tja test: \"Ready to\"", |b| {
         b.iter(|| tja_file_bench(black_box(ready_to)))
     });
 }
 
-pub fn tja_benchmark_with_read_str(c: &mut Criterion) {
-    c.bench_function("tja test: \"Ready to\", reading file at runtime", |b| {
-        b.iter(|| {
-            let ready_to =
-                black_box(std::fs::read_to_string("example-tracks/Ready To/Ready to.tja").unwrap());
-            tja_file_bench(black_box(&ready_to));
-        })
-    });
-}
-
 pub fn full_tja_benchmark(c: &mut Criterion) {
-    let ready_to = include_str!("../example-tracks/Ready To/Ready to.tja");
+    let ready_to = include_str!("../example-tracks/Ready to/Ready to.tja");
 
     c.bench_function("tja test: \"Ready to\", full parse", |b| {
         b.iter(|| parse_tja_file(black_box(ready_to)).unwrap());
@@ -29,7 +19,6 @@ pub fn full_tja_benchmark(c: &mut Criterion) {
 criterion_group!(
     benches,
     tja_benchmark,
-    tja_benchmark_with_read_str,
     full_tja_benchmark
 );
 criterion_main!(benches);
