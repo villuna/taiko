f44e79f4356569ab3fc979f164b67faf1f26838f
Luna Borella <ulfrthewolf@gmail.com>
2023-08-06 02:28:09 +1000

    Started rewriting the parser to be more manual and less error-prone

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -27,6 +27,7 @@ use nom::sequence::{delimited, pair, preceded, separated_pair, terminated};
 use nom::{Finish, IResult, Parser};
 
 mod test;
+mod tja_parser;
 
 /// An enum that represents a single unit of information in a TJA file.
 /// This is either a metadata tag, or an entire specification of the notes in
@@ -577,6 +578,7 @@ fn construct_difficulty<'a>(
     let mut bpm = get_parsed_metadata::<f32>(metadata, "BPM", Some(DEFAULT_BPM))?;
     let offset = get_parsed_metadata::<f32>(metadata, "OFFSET", Some(0.0))?;
     let init_scroll_speed = get_parsed_metadata::<f32>(metadata, "HEADSCROLL", Some(1.0))?;
+    let mut unscaled_scroll = init_scroll_speed;
     let mut scroll_speed = init_scroll_speed * bpm / DEFAULT_BPM;
     let mut balloon_count = 0;
 
@@ -604,6 +606,7 @@ fn construct_difficulty<'a>(
                     bpm = *new_bpm;
                     seconds_per_measure = 60.0 * signature * 4.0 / bpm;
                     seconds_per_note = seconds_per_measure / notes_in_measure as f32;
+                    scroll_speed = init_scroll_speed * (unscaled_scroll) * bpm / DEFAULT_BPM
                 }
                 TrackCommand::Measure(num, den) => {
                     signature = *num as f32 / *den as f32;
@@ -612,7 +615,8 @@ fn construct_difficulty<'a>(
                 }
                 TrackCommand::Delay(t) => time += *t,
                 TrackCommand::Scroll(s) => {
-                    scroll_speed = init_scroll_speed * (*s) * bpm / DEFAULT_BPM
+                    scroll_speed = init_scroll_speed * (*s) * bpm / DEFAULT_BPM;
+                    unscaled_scroll = *s;
                 }
                 TrackCommand::GogoStart => {}
                 TrackCommand::GogoEnd => {}
---
+++ src/parser/tja_parser.rs
@@ -0,0 +1,64 @@
+use nom::{bytes::complete::{take_while1, tag, is_not}, sequence::separated_pair, IResult, combinator::opt, Parser};
+
+use crate::track::Song;
+
+#[derive(Debug)]
+pub enum TJAParseError {
+    SyntaxError,
+}
+
+impl<I> From<nom::error::Error<I>> for TJAParseError {
+    fn from(value: nom::error::Error<I>) -> Self {
+        TJAParseError::SyntaxError
+    }
+}
+
+fn parse_metadata_pair(input: &str) -> IResult<&str, (&str, &str)> {
+    separated_pair(
+        take_while1(|c| ('A'..='Z').contains(&c)),
+        tag(":"),
+        opt(is_not("\r\n")).map(|value| value.unwrap_or("")),
+    )(input)
+}
+
+pub fn parse_tja_file(input: &str) -> Result<(), (TJAParseError, usize)> {
+    let lines = input.lines().enumerate().filter_map(|(i, line)| {
+        // This seems to be necessary as a lot of tja files have the utf-16 alignment character at
+        // the beginning. But as far as i'm aware, are not utf-16? If there's a satisfying
+        // conclusion to this problem, I would love to know it.
+        let mut line = line.strip_prefix('\u{feff}').unwrap_or(line);
+
+        // Remove comments
+        if let Some(i) = line.find("//") {
+            line = &line[0..i];
+        }
+
+        let line = line.trim();
+
+        if line.is_empty() {
+            None
+        } else {
+            Some((i, line))
+        }
+    });
+
+    for (i, line) in lines {
+        println!("{i}: {:?}", parse_metadata_pair(line));
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse_file() {
+        parse_tja_file(include_str!(
+            "../../songs/Mata Saitama 2000/Mata Saitama 2000.tja"
+        ))
+        .unwrap();
+        assert!(false);
+    }
+}
